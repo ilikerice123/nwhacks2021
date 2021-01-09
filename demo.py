d679b0f27469ab737cef49cad75c7fd991ed3e40
Charles <charlesbai321@gmail.com>
2021-01-09 14:45:51 -0800

    first commit!

---
+++ demo.py
@@ -0,0 +1,11 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+# binging with babish video on potstickers: https://www.youtube.com/watch?v=iC1rvXPt_rE
+video_id = 'iC1rvXPt_rE'
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+text = ''
+for subtitle in transcript:
+    text += subtitle['text'] + ' '
+
+print(text)
---
+++ transform_ingredients.py
@@ -0,0 +1,14 @@
+import json
+ingredients = set([])
+
+with open('train.json') as f:
+    data = json.load(f)
+
+for recipe in data:
+    for ingredient in recipe['ingredients']:
+        if ingredient not in ingredients:
+            ingredients.add(ingredient)
+
+parsed = list(ingredients)
+with open('ingredients.json', 'w') as fi:
+    json.dump(parsed, fi)
