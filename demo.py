05c86bfda943e212040056b342fc615ad87b4b47
Anthony Buonassisi <anthonybuonassisi@gmail.com>
2021-01-09 15:45:56 -0800

    Get some verbs from Babish

--- demo.py
+++ demo.py
@@ -3,12 +3,18 @@ import nltk
 
 # binging with babish video on potstickers: https://www.youtube.com/watch?v=iC1rvXPt_rE
 video_id = 'iC1rvXPt_rE'
-transcript = YouTubeTranscriptApi.get_transcript(video_id)
+lotr_special = 'g09731mD-s8'
+transcript = YouTubeTranscriptApi.get_transcript(lotr_special)
+
+def getVerbs():
+	tokens = nltk.word_tokenize(text)
+	tagged = nltk.pos_tag(tokens)
+	for tag in tagged:
+		if tag[1] == "VB":
+			print(tag[0].lower())
 
 text = ''
 for subtitle in transcript:
     text += subtitle['text'] + ' '
+getVerbs()
 
-print(text)
-
-# def ingredients(transcript):
