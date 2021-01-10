45fd8d712b7c6b9fa683fde9e1da81ce643d2691
kelvinkoon <kkoon@shaw.ca>
2021-01-09 18:00:46 -0800

    Add word frequency assets

--- demo.py
+++ demo.py
@@ -3,7 +3,8 @@ from ingredients import Ingredients
 
 # basics with babish video on burgers: https://www.youtube.com/watch?v=iC1rvXPt_rE
 video_id = 'nbCgfiqq-5c'
-transcript = YouTubeTranscriptApi.get_transcript(video_id)
+test = '25Od-bcv6vM&list=PLopY4n17t8RBuyIohlCY9G8sbyXrdEJls&index=2'
+transcript = YouTubeTranscriptApi.get_transcript(test)
 import nltk
 
 def getVerbs():
@@ -19,10 +20,9 @@ ingreds = []
 for subtitle in transcript:
     stuff = db.parse_ingredients(subtitle['text'])
     ingreds += stuff
-print(ingreds)
+# print(ingreds)
 
 # wow
 text = ''
 text += subtitle['text'] + ' '
 getVerbs()
-
